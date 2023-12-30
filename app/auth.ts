a0a7927a842b05446b52e5e8ed139551de3a1efb
IBN-5100 <43194683+IBN-5100@users.noreply.github.com>
2023-12-30 02:53:23 -0800

    discord swap

--- app/auth.ts
+++ app/auth.ts
@@ -1,14 +1,17 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import DiscordProvider from 'next-auth/providers/discord';
+
+const scopes = ['identify', 'email']
 
 export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
   providers: [
-    GitHub({
+    DiscordProvider({
       clientId: process.env.OAUTH_CLIENT_KEY as string,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET as string
+      clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
+      authorization: {params: {scope: scopes.join(' ')}},
     })
   ],
   pages: {
