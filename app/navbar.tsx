c9e58e1f6daba78999f1b218cf38efd0dc650bbb
IBN-5100 <43194683+IBN-5100@users.noreply.github.com>
2023-12-31 03:06:18 +0000

    logo new test

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -28,27 +28,19 @@ export default function Navbar({ user }: { user: any }) {
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    className="text-gray-100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      width="100%"
-                      height="100%"
-                      rx="16"
-                      fill="currentColor"
+                    <a href="/dashboard" className="flex items-center space-x-2 group">
+                      <Image
+                      src="/app/img/dseal.png" 
+                      alt="Logo" 
+                      width={32}
+                      height={32}
+                      className="rounded-full" 
                     />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                      fill="black"
-                    />
-                  </svg>
+                      <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
+                      <span className="text-sm font-medium text-gray-500 group-hover:text-gray-900 transition-all duration-300 opacity-0 group-hover:opacity-100">
+                        Dashboard
+                      </span>
+                    </a>
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                   {navigation.map((item) => (
